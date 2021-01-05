1d32c12fcf0ca75f9c99b30a34f9371b8ffcdd58
Lucas Rodrigo Frederico <55282018+lucasfrederico@users.noreply.github.com>
2021-01-05 20:51:02 -0300

    feat: escrevendo dados no Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
-  Firestore.instance.collection('col').document('doc').setData({'texto': 'daniel'});
+  Firestore.instance.collection('mensagens').document('L4dYrpooFTN50GKIUkgV').collection('arquivos').document().setData({
+    'arqname': 'foto.png'
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -11,12 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Container()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Container());
   }
 }
