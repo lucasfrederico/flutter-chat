a5594b1e0dc001bde867219c655922f661a7a2e5
Lucas Rodrigo Frederico <55282018+lucasfrederico@users.noreply.github.com>
2021-01-05 20:34:50 -0300

    feat: configurando o projeto para se comunicar com o Firebase

---
+++ android/app/src/main/kotlin/me/lucasfrederico/flutter_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.lucasfrederico.flutter_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+  Firestore.instance.collection('col').document('doc').setData({'texto': 'daniel'});
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Container()
+    );
+  }
+}
